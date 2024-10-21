7ea8a29be8f0df288d73cd44b6cfd0117b2374fc
Victor Sobrinho <victorsobrinho93@gmail.com>
2024-10-21 10:53:47 -0300

    tidied up the js, finished styling. Should be final.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
-function Question(element) {
-  const iconPlus = "./assets/images/icon-plus.svg";
-  const iconMinus = "./assets/images/icon-minus.svg";
-  this.isExpanded = false;
-  this.element = element;
-  this.questionRow = this.element.querySelector(".question");
-  this.answer = this.element.querySelector(".answer");
+function Question(target) {
+  this.state = false;
+  this.question = target.querySelector(".question");
+  this.answer = target.querySelector(".answer");
   this.answer.style.display = "none";
   this.icon = document.createElement("img");
-  this.icon.setAttribute("src", iconPlus);
-  this.questionRow.appendChild(this.icon);
+  this.icon.setAttribute("src", "./assets/images/icon-plus.svg");
+  this.question.appendChild(this.icon);
 
   this.setState = function () {
-    if (this.isExpanded) {
-      this.questionRow.querySelector("img").setAttribute("src", iconMinus);
-      this.element.querySelector(".answer").style.display = "flex";
+    this.state = !this.state;
+    if (this.state) {
+      this.question
+        .querySelector("img")
+        .setAttribute("src", "./assets/images/icon-minus.svg");
+      this.answer.style.display = "flex";
     } else {
-      this.questionRow.querySelector("img").setAttribute("src", iconPlus);
-      this.element.querySelector(".answer").style.display = "none";
+      this.question
+        .querySelector("img")
+        .setAttribute("src", "./assets/images/icon-plus.svg");
+      this.answer.style.display = "none";
     }
   };
 
-  this.element.addEventListener("click", (e) => {
-    this.isExpanded = !this.isExpanded;
-    this.setState();
-  });
+  target.addEventListener("click", () => this.setState());
 }
 
-Array.from(document.getElementsByClassName("question-block")).forEach(
+Array.from(document.getElementsByClassName("qa-section")).forEach(
   (e) => new Question(e)
 );
